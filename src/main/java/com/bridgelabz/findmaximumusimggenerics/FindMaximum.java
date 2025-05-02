43ed38e4c249aebf37507f21a69f77b50c315d81
vinayg2509 <vinayg2509@gmail.com>
2025-05-02 17:00:51 +0530

    Added method

--- src/main/java/com/bridgelabz/findmaximumusimggenerics/FindMaximum.java
+++ src/main/java/com/bridgelabz/findmaximumusimggenerics/FindMaximum.java
@@ -15,4 +15,18 @@ public class FindMaximum
         }
         return max;
     }
+
+    public  Float findFloatMax(Float num1,Float num2,Float num3)
+    {
+        Float max=num1;
+        if(num2.compareTo(max)>0)
+        {
+            max=num2;
+        }
+        if(num3.compareTo(max)>0)
+        {
+            max=num3;
+        }
+        return max;
+    }
 }
