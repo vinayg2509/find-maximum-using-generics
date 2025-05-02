278a00b7fda2367759e3233b9847b4c391ea5439
vinayg2509 <vinayg2509@gmail.com>
2025-05-02 12:51:12 +0530

    [Vinay] Implemented: Method to ensure maximum value is identified in first, second, and third positions

--- src/test/java/com/bridgelabz/findmaximumusimggenerics/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/findmaximumusimggenerics/FindMaximumTest.java
@@ -17,4 +17,72 @@ class FindMaximumTest
 
         System.out.println("Maximum value found: " + actualResult);
     }
+
+    // TC 1.1: Max at 1st position
+    @Test
+    public void findIntegerMaxAtFirstPosition()
+    {
+        Integer a=40;
+        Integer b=00;
+        Integer c=9;
+        if(a.compareTo(b)>0 && a.compareTo(c)>0)
+        {
+            Integer actualResult= findMaximum.findIntegerMax(a,b,c);
+            assertEquals(a,actualResult);
+
+            System.out.println("TC 1.1 - Inputs: a = " + a + ", b = " + b + ", c = " + c);
+            System.out.println("Maximum value found: " + actualResult);
+
+        }
+        else {
+            fail("Test data invalid: first number is not the maximum.");
+        }
+
+    }
+
+    // TC 1.2: Max at 2nd position
+    @Test
+    public void findIntegerMaxAtSecondPosition()
+    {
+
+
+        Integer a = 40;
+        Integer b = 100;
+        Integer c = 9;
+        Integer actualResult = findMaximum.findIntegerMax(a, b, c);
+        if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
+
+            assertEquals(b, actualResult);
+
+            System.out.println("TC 1.2 - Inputs: a = " + a + ", b = " + b + ", c = " + c);
+            System.out.println("Maximum value found: " + actualResult);
+
+        } else {
+            fail("Test data invalid: second number is not the maximum.");
+        }
+
+    }
+    // TC 1.3: Max at 3rd position
+    @Test
+    public void findIntegerMaxAtThirdPosition()
+    {
+
+        Integer a = 40;
+        Integer b = 100;
+        Integer c = 900;
+        Integer actualResult = findMaximum.findIntegerMax(a, b, c);
+
+        if (c.compareTo(a) > 0 && c.compareTo(b) > 0) {
+
+            assertEquals(c, actualResult);
+
+            System.out.println("TC 1.3 - Inputs: a = " + a + ", b = " + b + ", c = " + c);
+            System.out.println("Maximum value found: " + actualResult);
+
+        } else {
+            fail("Test data invalid: third number is not the maximum.");
+        }
+
+    }
+
 }
