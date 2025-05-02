5889324fb0fa45187a3703306c23d3e8367050c3
vinayg2509 <vinayg2509@gmail.com>
2025-05-02 12:43:19 +0530

    [Vinay] Implemented: Method to findMax() and its test method

--- src/main/java/com/bridgelabz/findmaximumusimggenerics/FindMaximum.java
+++ src/main/java/com/bridgelabz/findmaximumusimggenerics/FindMaximum.java
@@ -1,4 +1,32 @@
 package com.bridgelabz.findmaximumusimggenerics;
 
-public class FindMaximum {
+public class FindMaximum
+{
+    public  Integer findIntegerMax(Integer num1,Integer num2,Integer num3)
+    {
+        Integer max=num1;
+        if(num2.compareTo(max)>0)
+        {
+            max=num2;
+        }
+        if(num3.compareTo(max)>0)
+        {
+            max=num3;
+        }
+        return max;
+    }
+
+    public  Float findFloatMax(Float num1,Float num2,Float num3)
+    {
+        Float max=num1;
+        if(num2.compareTo(max)>0)
+        {
+            max=num2;
+        }
+        if(num3.compareTo(max)>0)
+        {
+            max=num3;
+        }
+        return max;
+    }
 }
--- src/test/java/com/bridgelabz/findmaximumusimggenerics/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/findmaximumusimggenerics/FindMaximumTest.java
@@ -1,9 +1,29 @@
 package com.bridgelabz.findmaximumusimggenerics;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FindMaximumTest
 {
+    FindMaximum findMaximum= new FindMaximum();
+    //UC-1 method to find Integer max
+    @Test
+    public void findIntegerMax()
+    {
+        Integer actualResult= findMaximum .findIntegerMax(10,40,30);
+        assertEquals(40,actualResult);
 
+        System.out.println("Maximum value found: " + actualResult);
+    }
+
+
+    //UC-2 method to find Float max
+    @Test
+    public void findFloatMax()
+    {
+        Float actualResult= findMaximum .findFloatMax(100.0f,400.5f,30.3f);
+        assertEquals(400.5f,actualResult);
+
+        System.out.println("Maximum value found: " + actualResult);
+    }
 
 }
