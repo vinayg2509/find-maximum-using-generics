1febf3ef35f0caa9ea6820071ba59d5111ba681b
vinayg2509 <vinayg2509@gmail.com>
2025-05-02 12:38:15 +0530

    [Vinay] Added: FindMaximum and FindMaximumTest classes, and configured pom.xml with JUnit dependency

---
+++ src/main/java/com/bridgelabz/findmaximumusimggenerics/FindMaximum.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.findmaximumusimggenerics;
+
+public class FindMaximum {
+}
---
+++ src/test/java/com/bridgelabz/findmaximumusimggenerics/FindMaximumTest.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.findmaximumusimggenerics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindMaximumTest
+{
+
+
+}
