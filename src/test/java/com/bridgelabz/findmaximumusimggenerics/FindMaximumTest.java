c30f3e6f560d7a3e50c8cd77e7e4cd6b04bc6355
vinayg2509 <vinayg2509@gmail.com>
2025-05-03 15:11:29 +0530

    [Vinay]Refactor:modify logic in all three methods for improved clarity

--- src/test/java/com/bridgelabz/findmaximumusimggenerics/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/findmaximumusimggenerics/FindMaximumTest.java
@@ -4,92 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FindMaximumTest
+class gitFindMaximumTest
 {
 
     FindMaximum findMaximum= new FindMaximum();
-    //UC-1 method to find Integer max
-    @Test
-    public void findIntegerMax()
-    {
-        Integer actualResult= findMaximum .findIntegerMax(10,40,30);
-        assertEquals(40,actualResult);
-
-        System.out.println("Maximum value found: " + actualResult);
-    }
-
-    // TC 1.1: Max at 1st position
-    @Test
-    public void findIntegerMaxAtFirstPosition()
-    {
-        Integer a=40;
-        Integer b=00;
-        Integer c=9;
-        if(a.compareTo(b)>0 && a.compareTo(c)>0)
-        {
-            Integer actualResult= findMaximum.findIntegerMax(a,b,c);
-            assertEquals(a,actualResult);
-
-            System.out.println("TC 1.1 - Inputs: a = " + a + ", b = " + b + ", c = " + c);
-            System.out.println("Maximum value found: " + actualResult);
-
-        }
-        else {
-            fail("Test data invalid: first number is not the maximum.");
-        }
-
-    }
-
-    // TC 1.2: Max at 2nd position
-    @Test
-    public void findIntegerMaxAtSecondPosition()
-    {
-
-
-        Integer a = 40;
-        Integer b = 100;
-        Integer c = 9;
-        Integer actualResult = findMaximum.findIntegerMax(a, b, c);
-        if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
-
-            assertEquals(b, actualResult);
-
-            System.out.println("TC 1.2 - Inputs: a = " + a + ", b = " + b + ", c = " + c);
-            System.out.println("Maximum value found: " + actualResult);
-
-        } else {
-            fail("Test data invalid: second number is not the maximum.");
-        }
-
-    }
-    // TC 1.3: Max at 3rd position
-    @Test
-    public void findIntegerMaxAtThirdPosition()
-    {
-
-        Integer a = 40;
-        Integer b = 100;
-        Integer c = 900;
-        Integer actualResult = findMaximum.findIntegerMax(a, b, c);
-
-        if (c.compareTo(a) > 0 && c.compareTo(b) > 0) {
-
-            assertEquals(c, actualResult);
-
-            System.out.println("TC 1.3 - Inputs: a = " + a + ", b = " + b + ", c = " + c);
-            System.out.println("Maximum value found: " + actualResult);
-
-        } else {
-            fail("Test data invalid: third number is not the maximum.");
-        }
-
-    }
 
     public void findFloatMax()
     {
         Float actualResult= findMaximum .findFloatMax(10.0f,40.9f,30.3f);
         assertEquals(40.9f,actualResult);
-
         System.out.println("Maximum value found: " + actualResult);
     }
     // TC 1.1: Max at 1st position
@@ -99,19 +22,8 @@ class FindMaximumTest
         Float a=400.0f;
         Float b=00.5f;
         Float c=90.f;
-        if(a.compareTo(b)>0 && a.compareTo(c)>0)
-        {
-            Float actualResult= findMaximum.findFloatMax(a,b,c);
-            assertEquals(a,actualResult);
-
-            System.out.println("TC 1.1 - Inputs: a = " + a + ", b = " + b + ", c = " + c);
-            System.out.println("Maximum value found: " + actualResult);
-
-        }
-        else {
-            fail("Test data invalid: first number is not the maximum.");
-        }
-
+        Float actualResult= findMaximum.findFloatMax(a,b,c);
+        assertEquals(a,actualResult,"Test data invalid: first number is not the maximum.");
     }
 
     // TC 1.2: Max at 2nd position
@@ -124,16 +36,7 @@ class FindMaximumTest
         Float b = 100f;
         Float c = 9f;
         Float actualResult = findMaximum.findFloatMax(a, b, c);
-        if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
-
-            assertEquals(b, actualResult);
-
-            System.out.println("TC 1.2 - Inputs: a = " + a + ", b = " + b + ", c = " + c);
-            System.out.println("Maximum value found: " + actualResult);
-
-        } else {
-            fail("Test data invalid: second number is not the maximum.");
-        }
+        assertEquals(b,actualResult,"Test data invalid: second number is not the maximum.");
 
     }
     // TC 1.3: Max at 3rd position
@@ -145,15 +48,7 @@ class FindMaximumTest
         Float b = 100f;
         Float c = 900f;
         Float actualResult = findMaximum.findFloatMax(a, b, c);
-        if (c.compareTo(a) > 0 && c.compareTo(b) > 0) {
-
-            assertEquals(c, actualResult);
-
-            System.out.println("TC 1.3 - Inputs: a = " + a + ", b = " + b + ", c = " + c);
-            System.out.println("Maximum value found: " + actualResult);
-        } else {
-            fail("Test data invalid: third number is not the maximum.");
-        }
+        assertEquals(c,actualResult,"Test data invalid: third number is not the maximum.");
 
     }
 }
